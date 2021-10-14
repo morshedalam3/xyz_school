b58acedad94d7c60702c3b14a87897f81355fb92
morshedalm3 <morshedhasan1969@gmail.com>
2021-10-14 20:47:31 +0600

    first commit

---
+++ src/Components/Home/Navber/Navber.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { Button, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Navber = () => {
+    return (
+       
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark"> 
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <li class="nav-link active " Link to="/">Home</li>
+          <li class="nav-link active " to="/About">About Us</li>
+          <li class="nav-link active  " to="/service">Service</li>
+          <li class="nav-link active " to="/contact">Contact</li>
+          </Nav>
+          <Button variant="outline-info">GET AN APPOINTMENT</Button>
+          </Navbar.Collapse>
+        </Navbar>
+        
+    );
+};
+
+export default Navber;
