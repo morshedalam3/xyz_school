0e72afb580b7f2382396ab1242e9a4e05eb7a50e
morshedalm3 <morshedhasan1969@gmail.com>
2021-10-28 13:09:29 +0600

    added

---
+++ src/Pages/OfficialActivities/OfficialActivites.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OfficialActivites = () => {
+    return (
+        <div>
+            <h1>official activites</h1>
+        </div>
+    );
+};
+
+export default OfficialActivites;
---
+++ src/Pages/OthersInformation/OthersInformation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OthersInformation = () => {
+    return (
+        <div>
+            <h1>others Information</h1>
+        </div>
+    );
+};
+
+export default OthersInformation;
